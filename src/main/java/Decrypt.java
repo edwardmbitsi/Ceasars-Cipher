c78b24c012e86206bd2d3f11a9ebdad2e4e11da1
edwardmbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-01-22 18:09:01 +0300

    Add files via upload

---
+++ src/main/java/Decrypt.java
@@ -0,0 +1,17 @@
+public class Decrypt {
+    public static void main(String[] args) {
+        int key = 6;
+        System.out.println("Enter a message");
+        Console myConsole = System.console();
+        String text = myConsole.readLine();
+        System.out.println("You entered " + text);
+        char[] chars = text.toCharArray();
+        for (char c : chars) {
+            c -= key;
+            System.out.print(c);
+
+        }
+
+
+    }
+}
