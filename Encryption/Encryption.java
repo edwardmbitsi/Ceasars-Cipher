72e9e1f7318dbb7d698fe04ddddc973174e5c459
edwardmbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-01-22 12:07:17 +0300

    Add files via upload

---
+++ Encryption/Encryption.java
@@ -0,0 +1,19 @@
+import java.io.Console;
+import java.util.Scanner;
+public class Encryption {
+    public static void main(String[] args) {
+        int key = 6;
+        System.out.println("Enter a message");
+        Console myConsole = System.console();
+        String text = myConsole.readLine();
+        System.out.println("You entered " + text);
+        char[] chars = text.toCharArray();
+        for (char c : chars) {
+            c += key;
+            System.out.print(c);
+
+        }
+
+
+    }
+}
